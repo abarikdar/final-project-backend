316d542d2c8c8167c1a1e1658b0b98b4304adb80
ceonz <45135014+ceonz@users.noreply.github.com>
2023-12-13 16:47:32 -0500

    add field on animals table and add mock data (#14)

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,47 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Task, Animal } = require('../database/models');
+const { Task, Animal } = require("../database/models");
 
-const ash = require('express-async-handler');
+const ash = require("express-async-handler");
 
 //read all task
-router.get('/', ash(async(req, res) => {
-  let tasks = await Task.findAll();
+router.get("/", ash(async (req, res) => {
+  let tasks = await Task.findAll({ include: Animal });
   res.status(200).json(tasks);
 }));
 
 //read one task by id
-router.get('/:id', ash(async(req, res) => {
-  let task = await Task.findByPk(req.params.id, {include: [Animal]});
+router.get("/:id", ash(async (req, res) => {
+  let task = await Task.findByPk(req.params.id, { include: [Animal] });
   res.status(200).json(task);
 }));
 
 //create new task
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   Task.create(req.body)
-    .then(createdTask => res.status(200).json(createdTask))
-    .catch(err => next(err));
+    .then((createdTask) => res.status(200).json(createdTask))
+    .catch((err) => next(err));
 });
 
 //delete task
-router.delete('/:id', function(req, res, next) {
+router.delete("/:id", function (req, res, next) {
   Task.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
     .then(() => res.status(200).json("Deleted a task!"))
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
 //update task
-router.put('/:id', ash(async(req, res) => {
-  await Task.update(req.body,
-        { where: {id: req.params.id} }
-  );
-  let task = await Task.findByPk(req.params.id);
+router.put("/:id", ash(async (req, res) => {
+  await Task.update(req.body, { where: { id: req.params.id } });
+  let task = await Task.findByPk(req.params.id, { include: [Animal] });
   res.status(201).json(task);
 }));
-
 
 module.exports = router;
